900a0e8a2f72016e16d151c5a76cbde53e98a17d
Onur Şavkı <42342278+onursavki@users.noreply.github.com>
2025-07-21 12:10:02 +0300

    winter-mod.js

--- winter-mod.js
+++ winter-mod.js
@@ -1,31 +1,31 @@
 // ATS ve ETS2 Kış Modu Bağlantı Verileri
 const modsData = {
     ats: [
-        { title: "Reklamsız İndir", url: "https://www.fatfreecartpro.com/i/14a32?card", description: "(Reklamsız indirme - 1$ maliyeti var. - Tüm eklentileri içerir.)" },
-        { title: "Ücretsiz İndir", url: "https://sharemods.com/xdz0c8y2akb2/frosty_ats_v5_4.scs.html", description: "(Reklamlarla ücretsiz indirin.)" },
+        { title: "Reklamsız İndir", url: "https://www.fatfreecartpro.com/i/14f4h?card", description: "(Reklamsız indirme - 1$ maliyeti var. - Tüm eklentileri içerir.)" },
+        { title: "Ücretsiz İndir", url: "https://sharemods.com/9140ydza3a5r/frosty_ats_v5_5.scs.html", description: "(Reklamlarla ücretsiz indirin.)" },
         
         // { isHeader: true, title: "Hata Düzeltme ve Geliştirmeler" }, // BAŞLIK
         // { title: "Geliştirilmiş Kar Yağışı", url: "https://sharemods.com/ypmy4tpi8w2k/frosty_v5_3_improved_snowfall_v2.scs.html", description: "(Frosty v5.3 için geliştirilmiş kar yağışı.)" },
         
         { isHeader: true, title: "İsteğe Bağlı Eklentiler" }, // BAŞLIK
-        { title: "Donmuş Tekerlekler", url: "https://sharemods.com/yp5ygu99saj9/frosty_wheels_5_4_ats.scs.html", description: "(Araç tekerleklerine buz ekler.)" },
-        { title: "Temiz Yollar", url: "https://sharemods.com/b49o9xs7rk8s/frosty_v5_4_ats_clean_roads.scs.html", description: "(Kar kaplamasını azaltır, daha temiz yollar sağlar.)" },
-        { title: "Kış Fiziği", url: "https://sharemods.com/g4przkwqdmrt/frosty_physics_ats_5_4.scs.html", description: "(Yolları biraz daha kaygan hale getirir.)" },
+        { title: "Donmuş Tekerlekler", url: "https://sharemods.com/hg41klm6pu61/frosty_wheels_5_5_ats.scs.html", description: "(Araç tekerleklerine buz ekler.)" },
+        { title: "Temiz Yollar", url: "https://sharemods.com/tqm7yu9uc6ra/frosty_v5_5_ats_clean_roads.scs.html", description: "(Kar kaplamasını azaltır, daha temiz yollar sağlar.)" },
+        { title: "Kış Fiziği", url: "https://sharemods.com/mxk2ktpzivsq/frosty_physics_ats_5_5.scs.html", description: "(Yolları biraz daha kaygan hale getirir.)" },
         
         { isHeader: true, title: "Diğer Paketler" }, // BAŞLIK
         { title: "Sezon Paketi", url: "https://www.fatfreecartpro.com/i/12dqe?card", description: "(İlkbahar, Yaz, Erken Sonbahar, Geç Sonbahar ve Kış'ı içerir - 2,99$ maliyeti var.)" }
     ],
     ets2: [
-        { title: "Reklamsız İndir", url: "https://www.fatfreecartpro.com/i/149ut?card", description: "(Reklamsız indirme - 1$ maliyeti var. - Tüm eklentileri içerir.)" },
-        { title: "Ücretsiz İndir", url: "https://sharemods.com/wnvjr6h8ujzr/frosty_v10_1.scs.html", description: "(Reklamlarla ücretsiz indirin.)" },
+        { title: "Reklamsız İndir", url: "https://www.fatfreecartpro.com/i/14et3?card", description: "(Reklamsız indirme - 1$ maliyeti var. - Tüm eklentileri içerir.)" },
+        { title: "Ücretsiz İndir", url: "https://sharemods.com/412fu2gcyz7u/frosty_v10_2.scs.html", description: "(Reklamlarla ücretsiz indirin.)" },
         
         // { isHeader: true, title: "Hata Düzeltme ve Geliştirmeler" }, // BAŞLIK
         // { title: "İyileştirme ve Düzeltme Paketi", url: "https://sharemods.com/nm9rfm8fgcqx/frosty_v10_improvements_fixes_pack_v2.scs.html", description: "(Frosty v10 için kar yağışı ve hata düzeltmeleri iyileştirildi.)" },
         
         { isHeader: true, title: "İsteğe Bağlı Eklentiler" }, // BAŞLIK
-        { title: "Donmuş Tekerlekler", url: "https://sharemods.com/jybsnohhtvqj/frosty_wheels_10_1.scs.html", description: "(Araç tekerleklerine buz ekler.)" },
-        { title: "Temiz Yollar", url: "https://sharemods.com/dk57mqhph7hu/frosty_v10_1_clean_roads.scs.html", description: "(Kar kaplamasını azaltır, daha temiz yollar sağlar.)" },
-        { title: "Kış Fiziği", url: "https://sharemods.com/4y02p8qyudkk/frosty_physics_10_1.scs.html", description: "(Yolları biraz daha kaygan hale getirir.)" },
+        { title: "Donmuş Tekerlekler", url: "https://sharemods.com/l30l2zfwrndy/frosty_wheels_10_2.scs.html", description: "(Araç tekerleklerine buz ekler.)" },
+        { title: "Temiz Yollar", url: "https://sharemods.com/9uixps8lqrrz/frosty_v10_2_clean_roads.scs.html", description: "(Kar kaplamasını azaltır, daha temiz yollar sağlar.)" },
+        { title: "Kış Fiziği", url: "https://sharemods.com/mqk7rzon4eb2/frosty_physics_10_2.scs.html", description: "(Yolları biraz daha kaygan hale getirir.)" },
         
         { isHeader: true, title: "Diğer Paketler" }, // BAŞLIK
         { title: "Sezon Paketi", url: "https://www.fatfreecartpro.com/i/12edw?card", description: "(İlkbahar, Yaz, Erken Sonbahar, Geç Sonbahar ve Kış'ı içerir - 2,99$ maliyeti var.)" }
@@ -33,28 +33,28 @@ const modsData = {
 };
 
 // Versiyonları tanımla
-const atsVersion = "5.4";
-const ets2Version = "10.1";
+const atsVersion = "5.5";
+const ets2Version = "10.2";
 
 // Oyun versiyonları
-const atsGameVersion = "1.54";
-const ets2GameVersion = "1.54";
+const atsGameVersion = "1.55";
+const ets2GameVersion = "1.55";
 
 // Dinamik başlıkları (h5) yerleştirme
 const atsTitles = {
-    atsFrosty: "frosty_ats_v5_4.scs",
-    atsWinter: "frosty_heavy_winter_v5_4.scs",
-    atsWheels: "frosty_wheels_5_4_ats.scs",
-    atsClean: "frosty_v5_4_clean_roads.scs",
-    atsPhysics: "frosty_physics_ats_5_4.scs"
+    atsFrosty: "frosty_ats_v5_5.scs",
+    atsWinter: "frosty_heavy_winter_v5_5.scs",
+    atsWheels: "frosty_wheels_5_5_ats.scs",
+    atsClean: "frosty_v5_5_clean_roads.scs",
+    atsPhysics: "frosty_physics_ats_5_5.scs"
 };
 
 const etsTitles = {
-    etsFrosty: "frosty_v10_1.scs",
-    etsWinter: "frosty_heavy_winter_v10_1.scs",
-    etsWheels: "frosty_wheels_10_1.scs",
-    etsClean: "frosty_v10_1_clean_roads.scs",
-    etsPhysics: "frosty_physics_10_1.scs"
+    etsFrosty: "frosty_v10_2.scs",
+    etsWinter: "frosty_heavy_winter_v10_2.scs",
+    etsWheels: "frosty_wheels_10_2.scs",
+    etsClean: "frosty_v10_2_clean_roads.scs",
+    etsPhysics: "frosty_physics_10_2.scs"
 };
 
 // h5 başlıklarını yerleştir
